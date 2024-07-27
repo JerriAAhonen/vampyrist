872d15e34edb5c23b09651f7b3335f89f3586201
Jerri Ahonen <jerri.ahonen@gmail.com>
2024-07-27 21:26:35 +0300

    Show highscore in mainmenu

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,13 +1,18 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
 	[SerializeField] private Button playButton;
+	[SerializeField] private TextMeshProUGUI highscore;
 
 	private void Awake()
 	{
 		playButton.onClick.AddListener(OnPlay);
+		var hs = PlayerPrefs.GetInt(LevelController.PP_Key_Highscore, -1);
+		highscore.text = $"Highscore level {hs}";
+		highscore.gameObject.SetActive(hs > -1);
 	}
 
 	private void OnPlay()
