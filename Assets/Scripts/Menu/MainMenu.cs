d3ec48cfc723df44da874f8edc416ef452f1ad3f
Jerri Ahonen <jerri.ahonen@gmail.com>
2024-07-30 16:54:45 +0300

    Fix menu

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -4,16 +4,26 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+	[SerializeField] private GameObject mainPage;
+	[SerializeField] private GameObject controlsPage;
 	[SerializeField] private DefaultButton playButton;
 	[SerializeField] private DefaultButton soundButton;
+	[SerializeField] private DefaultButton controlsButton;
+	[SerializeField] private DefaultButton backToMainMenuButton;
 	[SerializeField] private DefaultButton quitButton;
 	[SerializeField] private TextMeshProUGUI highscore;
 
 	private void Awake()
 	{
+		mainPage.SetActive(true);
+		controlsPage.SetActive(false);
+
 		playButton.OnClick.AddListener(OnPlay);
 		soundButton.OnClick.AddListener(OnSounds);
+		controlsButton.OnClick.AddListener(OnControls);
+		backToMainMenuButton.OnClick.AddListener(OnBackToMainPage);
 		quitButton.OnClick.AddListener(OnQuit);
+
 		var hs = PlayerPrefs.GetInt(LevelController.PP_Key_Highscore, -1);
 		highscore.text = $"Highscore level {hs}";
 		highscore.gameObject.SetActive(hs > -1);
@@ -27,6 +37,18 @@ public class MainMenu : MonoBehaviour
 	private void OnSounds()
 	{
 
+	}
+
+	private void OnControls()
+	{
+		mainPage.SetActive(false);
+		controlsPage.SetActive(true);
+	}
+
+	private void OnBackToMainPage()
+	{
+		mainPage.SetActive(true);
+		controlsPage.SetActive(false);
 	}
 
 	private void OnQuit()
